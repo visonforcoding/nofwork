9fb7c5c7ce7d91f0554d9565360bf48659adb47e
visonforcoding <caowenpeng1990@126.om>
2017-07-24 18:18:52 +0800

    first commit

---
+++ webroot/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * The Front Controller for handling every request
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ */
+echo 'hell,world';
+// for built-in server
+//define('APP_ROOT', dirname(__DIR__));
+//require APP_ROOT . '/vendor/autoload.php';
+//
+//use Monolog\Logger;
+//use Monolog\Handler\RotatingFileHandler;
+//
+//$log = new Logger('demaya');
+//
+//$log_base = APP_ROOT . '/logs/'; //日志根目录
+//$log->pushHandler(new RotatingFileHandler($log_base . 'debug' . '.log', 0, Logger::DEBUG));
+//$log->debug('test', $_SERVER);
+
+
