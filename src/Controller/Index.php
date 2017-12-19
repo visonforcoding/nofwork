4a6e5741b9519430cb20e16657bf4edd22a8ff05
visonforcoding <caowenpeng1990@126.com>
2017-12-19 12:27:52 +0800

    test hook

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -3,9 +3,9 @@ namespace App\Controller;
 
 class Index
 {
-    public function index()
-    {
+    public function index(){
 			\dmy\logger\Log::add('debug', '测试', []);
+			
         dump(__FILE__);
     }
 }
